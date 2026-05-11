7c25e73431bee6dfed23607c8d73c1f2e76a6e70
sutie <susususutie@gmail.com>
2026-05-11 22:20:51 +0800

    重构(types): 将store相关类型定义拆分至独立文件

---
+++ src/types/store.ts
@@ -0,0 +1,38 @@
+import PanZoom from "../helper/PanZoom";
+
+// 静态配置, 来自 props，初始化后不变（从 props 中获取初始化后恒定不变 ）
+export type StoreConfig = {
+  id: string;
+};
+
+// data 业务状态，中低频变化（秒级/分钟级）
+export type StoreData = {
+  minZoom: number; // 缩放百分比，整数
+  maxZoom: number;
+  initialZoom: number;
+  panZoom: PanZoom | null;
+};
+
+// reactive 实时状态，高频变化（帧级）
+export type StoreReactive = {
+  transform: {
+    x: number; // 位移 X
+    y: number; // 位移 Y
+    zoom: number; // 缩放百分比例，整数方便计算
+  };
+  // 其他交互状态
+  mousePosition: { x: number; y: number };
+  selectedPoint: { x: number; y: number } | null;
+  isPanning: boolean;
+};
+
+export type StoreStateType = StoreConfig & StoreData & StoreReactive;
+
+export type StoreAction =
+  | { type: "setZoom"; payload: number }
+  | { type: "transform"; payload: StoreReactive["transform"] }
+  | { type: "setInitialZoom"; payload: number }
+  | { type: "incrementZoom" }
+  | { type: "decrementZoom" }
+  | { type: "reset" }
+  | { type: "setPanZoom"; payload: PanZoom | null };
